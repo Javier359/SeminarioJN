978826f979e521e20a8d5c3506ee21ebef3872da
Javier Jimenez <javier.sniguel@gmail.com>
2025-05-28 00:39:34 -0300

    se crearon los scroll verticales del navbar

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import { Element } from "react-scroll";
 import Inicio from "./PaginaPrincipal/Inicio";
 import video from "../../public/videos/video-principal.mp4";
 import DescripcionApp from "./PaginaPrincipal/DescripcionApp";
 import ComoFunciona from "./ComoFunciona/ComoFunciona";
 import "../styles/Principal.css";
-import "../styles/App.css";
 
 const Principal = () => {
   return (
     <div className="pagina-principal">
-      <div className="seccion-video">
+      <Element name="inicio" className="seccion-video">
         <video className="background-video" autoPlay loop muted playsInline>
           <source src={video} type="video/mp4" />
           Tu navegador no soporta la etiqueta de video.
@@ -17,12 +16,17 @@ const Principal = () => {
         <div className="contenedor-principal">
           <Inicio />
         </div>
-      </div>
-      <DescripcionApp />
-       <ComoFunciona />
+      </Element>
+
+      <Element name="como-funciona">
+        <ComoFunciona />
+      </Element>
+
+      <Element name="descripcion">
+        <DescripcionApp />
+      </Element>
     </div>
   );
 };
-
 
 export default Principal;
