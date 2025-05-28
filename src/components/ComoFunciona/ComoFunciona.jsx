f7128e5fbf7ce355d80d2d36b0e87aa2061a9be3
Javier Jimenez <javier.sniguel@gmail.com>
2025-05-27 22:21:28 -0300

    se agrego el carrusel en el componente de como funciona y ademas se agregaron estilos provisorios para fijar las imagenes

--- src/components/ComoFunciona/ComoFunciona.jsx
+++ src/components/ComoFunciona/ComoFunciona.jsx
@@ -1,33 +1,45 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import ItemDetalle from "./ItemDetalle";
-import imagen from "../img/img-como-funciona.png";
-import "../../styles/Como-funciona.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "react-bootstrap/Image";
+import imagen1 from "../../../public/Imagenes-entrevistas/Portada-tipos-de-entrevistas.jpg";
+import Carousel from "react-bootstrap/Carousel";
 
-const ComoFunciona = () => {
+function UncontrolledExample() {
   return (
-    <Container className="my-5 text-center">
-      <h1 className="mb-4">Cómo Funciona</h1>
-      <Row className="d-flex align-items-stretch p-4 gap-4">
-        <Col
-          md={5}
-          className="d-flex align-items-center justify-content-center"
-        >
-          <img
-            src={imagen}
-            alt="imagen ia"
-            className="img-fluid rounded-4 shadow"
-            id="img-como-funciona"
-          />
-        </Col>
-        <Col md={6} className="d-flex align-items-center">
-          <div id="container-como-funciona" className="w-100">
-            <ItemDetalle />
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <Carousel>
+      <Carousel.Item>
+        <Image
+          src="/Imagenes-entrevistas/Portada-que-es-una-entrevista-2.jpg"
+          style={{ width: "100%", height: "500px", objectFit: "cover" }}
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="/Imagenes-entrevistas/Portada-tipos-de-entrevistas.jpg"
+          style={{ width: "100%", height: "500px", objectFit: "cover" }}
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          src="/Imagenes-entrevistas/entrevista-de-seleccion-1.jpg"
+          style={{ width: "100%", height: "500px", objectFit: "cover" }}
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
-};
+}
 
-export default ComoFunciona;
+export default UncontrolledExample;
