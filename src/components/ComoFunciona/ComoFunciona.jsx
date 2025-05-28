c26fa2b3834d8227c2d903e818fc4a8b432c6ed5
Javier Jimenez <javier.sniguel@gmail.com>
2025-05-27 23:23:39 -0300

    Agregamos el componente descripcion con su hoja de estilo

--- src/components/ComoFunciona/ComoFunciona.jsx
+++ src/components/ComoFunciona/ComoFunciona.jsx
@@ -1,19 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "react-bootstrap/Image";
-import imagen1 from "../../../public/Imagenes-entrevistas/Portada-tipos-de-entrevistas.jpg";
 import Carousel from "react-bootstrap/Carousel";
+import "../../styles/como-funciona.css";
 
 function UncontrolledExample() {
   return (
-    <Carousel>
+    <Carousel className="my-5" style={{ width: "100%" }}>
       <Carousel.Item>
         <Image
           src="/Imagenes-entrevistas/Portada-que-es-una-entrevista-2.jpg"
           style={{ width: "100%", height: "500px", objectFit: "cover" }}
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Carousel.Caption className="text-dark bg-light bg-opacity-75">
+          <h3>1.- Indicar el Puesto</h3>
+          <p>
+            Indique el Puesto al que quiera aplicar. "Ej. Desarrollador Java
+            Junior"
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -21,9 +24,12 @@ function UncontrolledExample() {
           src="/Imagenes-entrevistas/Portada-tipos-de-entrevistas.jpg"
           style={{ width: "100%", height: "500px", objectFit: "cover" }}
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption className="text-dark bg-light bg-opacity-75">
+          <h3>2.- Simulación Real:</h3>
+          <p>
+            La inteligencia artificial se encarga de recopilar una serie de
+            preguntas para el puesto indicado
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -31,10 +37,12 @@ function UncontrolledExample() {
           src="/Imagenes-entrevistas/entrevista-de-seleccion-1.jpg"
           style={{ width: "100%", height: "500px", objectFit: "cover" }}
         />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
+        <Carousel.Caption className="text-dark bg-light bg-opacity-75">
+          <h3>3.- Análisis:</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            La inteligencia artificial analiza tus expresiones vocales (Tú voz
+            en profundidad-Contenido de tu respuesta-Aspectos psicologicos). Es
+            decir que no solo analiza lo que dices, sino también como lo dices.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
