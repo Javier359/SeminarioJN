203dae6f0d381c428ea3111c1212256390bc579f
Javier Jimenez <javier.sniguel@gmail.com>
2025-05-28 00:29:53 -0300

    se agrego todo el contenido en inicio

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Inicio from "./PaginaPrincipal/Inicio";
 import video from "../../public/videos/video-principal.mp4";
+import DescripcionApp from "./PaginaPrincipal/DescripcionApp";
+import ComoFunciona from "./ComoFunciona/ComoFunciona";
+import "../styles/Principal.css";
+import "../styles/App.css";
 
 const Principal = () => {
   return (
-    <>
-      <div className="contenedor-principal"> <Inicio></Inicio></div>
-      <video className="background-video" autoPlay loop muted playsInline>
-        <source src={video} type="video/mp4" />
-        Tu navegador no soporta la etiqueta de video.
-      </video>
-    
-    </>
+    <div className="pagina-principal">
+      <div className="seccion-video">
+        <video className="background-video" autoPlay loop muted playsInline>
+          <source src={video} type="video/mp4" />
+          Tu navegador no soporta la etiqueta de video.
+        </video>
+        <div className="contenedor-principal">
+          <Inicio />
+        </div>
+      </div>
+      <DescripcionApp />
+       <ComoFunciona />
+    </div>
   );
 };
+
 
 export default Principal;
