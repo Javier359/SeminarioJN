1902b86dc15884d23ad7f55767e85bab885f39d0
Javier Jimenez <javier.sniguel@gmail.com>
2025-06-12 15:21:40 -0300

    se creo la card para reiniciar el contador de minutos y las preguntas. ademas se implemento el componente que contendra las preguntas

---
+++ src/components/Preguntas/PreguntasIndividuales.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreguntasIndividuales = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PreguntasIndividuales;
--- src/components/Preguntas/PregutasPrincipal.jsx
+++ src/components/Preguntas/PregutasPrincipal.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 const PregutasPrincipal = () => {
   return (
-    <Card>
-      <Card.Header>Featured</Card.Header>
+    <Container className="d-flex justify-content-center align-items-center flex-column vh-100 vw-100">
+    <Card className="d-flex text-center shadow-lg rounded-4 my-5 w-50" >
+      <Card.Header>ENTREVISTANDO</Card.Header>
       <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
+        <Card.Title>Desarrollador junior en Java</Card.Title>
         <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
+          Por favor responde a las preguntas que veras en pantala en el tiempo convenido.
         </Card.Text>
-        <Button variant="primary"> Go somewhere</Button>
+        <Button variant="danger">Reiniciar</Button>
       </Card.Body>
     </Card>
+    <Card className="d-flex text-center shadow-lg rounded-4 my-5 w-50" >
+      <Card.Header>Prueba de aqui asi tiene que quedar</Card.Header>
+      <Card.Body>
+        <Card.Title>Desarrollador junior en JavaScript</Card.Title>
+        <Card.Text>
+          Por favor responde a las preguntas que veras en pantala en el tiempo convenido.
+        </Card.Text>
+        <Button variant="danger">Reiniciar</Button>
+      </Card.Body>
+    </Card>
+    </Container>
   );
 };
 
